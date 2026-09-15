541a078ddf5b70dc7ad3382fa060e3da93f2a9b1
siefelkordy <seifemadelkordy@gmail.com>
2026-09-15 19:16:20 +0300

    Theoritical Q1

--- OOP-1/Program.cs
+++ OOP-1/Program.cs
@@ -2,6 +2,9 @@
 {
     internal class Program
     {
+        //Part 01 : Theoretical Questions//
+        //Q1a) When the copy is modified no change happens to the originalvalue as the struct is value type
+        //Q1b) When the copy is modified the original value also changes as the class is reference type
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
